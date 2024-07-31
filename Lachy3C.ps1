c7c6a972a3baf614f2ad25d965e79ca9acb87aba
Lachlan Davies <37570905+lachydavies25@users.noreply.github.com>
2024-07-31 22:13:51 +1000

    Create Lachy3C.ps1

---
+++ Lachy3C.ps1
@@ -0,0 +1,43 @@
+Set-PSDebug -Off
+# Bitwarden
+Write-Output "Installing Bitwarden"
+timeout /t 3 /nobreak
+winget install Bitwarden.Bitwarden --accept-package-agreements --accept-source-agreements -s winget
+
+# Microsoft Office
+Write-Output "Installing Microsoft Office"
+timeout /t 3 /nobreak
+winget install Microsoft.Office --accept-package-agreements --accept-source-agreements -s winget
+
+# Microsoft Teams
+Write-Output "Installing Microsoft Teams"
+timeout /t 3 /nobreak
+winget install Microsoft.Teams --accept-package-agreements --accept-source-agreements -s winget
+
+# Apple Music
+Write-Output "Installing Apple Music"
+timeout /t 3 /nobreak
+winget install 9PFHDD62MXS1 --accept-package-agreements --accept-source-agreements -s msstore
+
+# Apple Devices (For syncing apple devices)
+Write-Output "Installing Apple Devices"
+timeout /t 3 /nobreak
+winget install 9NP83LWLPZ9K --accept-package-agreements --accept-source-agreements -s msstore
+
+# Microsoft To Do
+Write-Output "Installing Microsoft To Do"
+timeout /t 3 /nobreak
+winget install 9NBLGGH5R558 --accept-package-agreements --accept-source-agreements -s msstore
+
+# Visual Studio Code
+Write-Output "Installing Visual Studio Code"
+timeout /t 3 /nobreak
+winget install XP9KHM4BK9FZ7Q --accept-package-agreements --accept-source-agreements -s winget
+
+# Company Portal
+Write-Output "Installing Company Portal"
+timeout /t 3 /nobreak
+winget install 9WZDNCRFJ3PZ --accept-package-agreements --accept-source-agreements -s msstore
+
+# Lets you check if all apps installed correctly
+pause
